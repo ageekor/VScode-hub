b147fdfcc94bc164306c4a694858b239a2f2ff0f
ageekor <2928153115@qq.com>
2019-03-09 12:59:51 +0800

    https://github.com/ageekor/VScode-hub

---
+++ dayup.py
@@ -0,0 +1,15 @@
+def dayUP(df):
+    dayup = 1
+    for i in range(365):
+        if i % 7 in [0,6]:
+            dayup = 0.99*dayup
+            
+        else:
+            dayup = (1+df)*dayup
+            
+    return dayup
+dayfactor = 0.01
+while dayUP(dayfactor) < 37.78:
+    dayfactor += 0.001
+
+print("工作日的努力参数是：{:.3f}".format(dayfactor))
